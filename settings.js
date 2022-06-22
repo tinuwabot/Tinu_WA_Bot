cafcbcfddc19c048070c62a062b8f7fda11937ad
tinuwabot <107972059+tinuwabot@users.noreply.github.com>
2022-06-22 08:17:05 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['94701566071']
+global.premium = ['94701566071']
+global.ownernomer = '94701566017'
+global.ownername = 'Tinu'
+global.botname = 'SL Tinu Tech Bot'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
-global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.myweb = 'https://www.youtube.com/c/SLTinuTech'
+global.packname = 'Tinu-Pack'
+global.author = '🐦Star Boy Tinu'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
